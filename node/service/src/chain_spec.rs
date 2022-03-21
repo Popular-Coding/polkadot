f56079c59a69c21dbbadab177b03abe091d36294
Svyatoslav Nikolsky <svyatonik@gmail.com>
2022-03-21 19:49:51 +0300

    revert council members = initial authorities

--- node/service/src/chain_spec.rs
+++ node/service/src/chain_spec.rs
@@ -1332,13 +1332,6 @@ pub fn polkadot_testnet_genesis(
 		},
 		phragmen_election: Default::default(),
 		democracy: polkadot::DemocracyConfig::default(),
-		council: polkadot::CouncilConfig {
-			members: initial_authorities
-				.iter()
-				.map(|x| x.0.clone())
-				.collect::<Vec<_>>(),
-			phantom: Default::default()
-		},
 		technical_committee: polkadot::TechnicalCommitteeConfig {
 			members: vec![],
 			phantom: Default::default(),
@@ -1432,13 +1425,6 @@ pub fn kusama_testnet_genesis(
 		},
 		phragmen_election: Default::default(),
 		democracy: kusama::DemocracyConfig::default(),
-		council: kusama::CouncilConfig {
-			members: initial_authorities
-				.iter()
-				.map(|x| x.0.clone())
-				.collect::<Vec<_>>(),
-			phantom: Default::default(),
-		},
 		technical_committee: kusama::TechnicalCommitteeConfig {
 			members: vec![],
 			phantom: Default::default(),
